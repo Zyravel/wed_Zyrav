f6f80de4ffe7f4d9b40bd4160508e8faa9fc1769
Zyravel <mozyrik@mail.ru>
2024-07-14 00:46:25 +0300

    qq

--- wtf_les.py
+++ wtf_les.py
@@ -1,23 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import sqlite3
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import DataRequired
 
-con = sqlite3.connect('web_base.db')
+
+class MyForm(FlaskForm):
+    id = IntegerField('id', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    bill = IntegerField('bill', validators=[DataRequired()])
+con = sqlite3.connect('web_base.db', check_same_thread=False)
 cur = con.cursor()
-res = cur.execute(f'select * from meat')
-result = res.fetchall()
-con.commit
+# res = cur.execute(f'select * from meat')
+# result = res.fetchall()
+# con.commit
 app = Flask(__name__)
 
+# @app.route('/add_u')
+# def add_u():
+#     id = request.args.get('id')
+#     name = request.args.get('name')
+#     bill = request.args.get('bill')
+#     new_text = (id, name, bill)
 
-@app.route("/")
-def hello():
-    return render_template('main.html')
+#     cur.execute(f'INSERT INTO meat(id,name,bill) VALUES (?, ?, ?)', new_text)
+#     con.commit()
+#     res = curs.execute(f'SELECT * FROM meat')
+#     return res.fetchall()
 
-@app.route('/x')
-def re():
 
-    return render_template('test.html', t = result)
+# @app.route("/")
+# def hello():
+#     return render_template('main.html')
+
+# @app.route('/x')
+# def re():
+
+#     return render_template('test.html', t = result)
+
+@app.route("/wt", methods = ['GET', 'POST'])
+def wt():
+    form = MyForm()
+    if form.validate_on_submit():
+        res_id = form.data['id']
+        res_name = form.data['name']
+        res_bill = form.data['bill']
+        new_text = (res_id, res_name, res_bill)
+
+        cur.execute(f'INSERT INTO meat(id, name, bill) VALUES (?, ?, ?)', new_text)
+        con.commit()
+        return "ok"
+    return render_template('wtftest.html', form = form)
 
 
 if __name__ == '__main__':
+    app.config['WTF_CSRF_ENABLED'] = False
     app.run()
