aa0e7c847c476210d391fc96150dc1212e9ba0d6
Zyravel <mozyrik@mail.ru>
2024-07-13 14:54:39 +0300

    les5.2

--- web_lesson.py
+++ web_lesson.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+import sqlite3
+import json
+
+con = sqlite3.connect("web_base.db",check_same_thread=False)
+curs = con.cursor()
+# res = curs.execute(f'SELECT * FROM meat')
+# print(res.fetchall())
+
 
 app = Flask(__name__)
 
@@ -8,8 +16,27 @@ def hi():
 
 
 
-@app.route("/user/<x>")
-def hx(x):
-    return f'hellow, {x}'
+@app.route("/user/<n>")
+def hx(n):
+    res = curs.execute(f'SELECT * FROM meat WHERE id = {n}')
+    r = res.fetchone()
+
+    return f'{r[1]}'
+
+
+@app.route('/add_u')
+def add_u():
+    id = request.args.get('id')
+    name = request.args.get('name')
+    bill = request.args.get('bill')
+    new_text = (id, name, bill)
+    # res = curs.execute(f'SELECT * FROM meat')
+    # return res.fetchall()
+    curs.execute(f'INSERT INTO meat(id,name,bill) VALUES (?, ?, ?)', new_text)
+    con.commit()
+    res = curs.execute(f'SELECT * FROM meat')
+    return res.fetchall()
+    # return "id = {};name = {}; bill = {}".format(id, name, bill) 
+
 
 app.run()
