e2753c161ea760fde8c045afe5809521c033417e
Zyravel <mozyrik@mail.ru>
2024-07-14 15:10:46 +0300

    qqq

--- wtf_les.py
+++ wtf_les.py
@@ -3,7 +3,7 @@ import sqlite3
 from flask_wtf import FlaskForm
 from wtforms import *
 from wtforms.validators import DataRequired
-
+from flask_sqlalchemy import SQLAlchemy
 
 class MyForm(FlaskForm):
     id = IntegerField('id', validators=[DataRequired()])
@@ -11,10 +11,38 @@ class MyForm(FlaskForm):
     bill = IntegerField('bill', validators=[DataRequired()])
 con = sqlite3.connect('web_base.db', check_same_thread=False)
 cur = con.cursor()
+
+
+
+
 # res = cur.execute(f'select * from meat')
 # result = res.fetchall()
 # con.commit
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    res = cur.execute(f'SELECT * FROM meat')
+    return res.fetchall()
+
+
+    # return render_template('main.html')
+
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///web_base.db'
+# db = SQLAlchemy(app)
+# class test_meat(db.Model):
+#     __tablename__ = 'meat'
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(30))
+#     bill = db.Column(db.Integer)
+#     def __init__(self, id, name, bill):
+#         self.id = id
+#         self.name = name
+#         self.bill = bill
+
+
 
 # @app.route('/add_u')
 # def add_u():
@@ -38,21 +66,28 @@ app = Flask(__name__)
 
 #     return render_template('test.html', t = result)
 
-@app.route("/wt", methods = ['GET', 'POST'])
-def wt():
-    form = MyForm()
-    if form.validate_on_submit():
-        res_id = form.data['id']
-        res_name = form.data['name']
-        res_bill = form.data['bill']
-        new_text = (res_id, res_name, res_bill)
+# @app.route("/wt", methods = ['GET', 'POST'])
+# def wt():
+#     form = MyForm()
+#     if form.validate_on_submit():
+#         res_id = form.data['id']
+#         res_name = form.data['name']
+#         res_bill = form.data['bill']
+#         # new_text = (res_id, res_name, res_bill)
+        
+#         new_meat = test_meat(res_id, res_name, res_bill)
 
-        cur.execute(f'INSERT INTO meat(id, name, bill) VALUES (?, ?, ?)', new_text)
-        con.commit()
-        return "ok"
-    return render_template('wtftest.html', form = form)
+#         db.session.add(new_meat)
+#         db.session.commit()
+#         x = test_meat.query.all()
+#         for i in x:
+#             print (i.name)
+#         # cur.execute(f'INSERT INTO meat(id, name, bill) VALUES (?, ?, ?)', new_text)
+#         # con.commit()
+#         return "ok"
+#     return render_template('wtftest.html', form = form)
 
 
 if __name__ == '__main__':
-    app.config['WTF_CSRF_ENABLED'] = False
+    # app.config['WTF_CSRF_ENABLED'] = False
     app.run()
